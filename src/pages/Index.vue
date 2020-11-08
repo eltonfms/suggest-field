4af6f3e70cd9b6c3f58d79928a9646e0b55040fe
Elton Fagner <eltonfms@gmail.com>
2020-11-08 20:17:40 -0300

    Plugin Vue Simple Suggest

---
+++ src/pages/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-page class="flex flex-center column">
+    <vue-simple-suggest
+      v-model="chosen"
+      :list="simpleSuggestionList"
+      :filter-by-query="true">
+    </vue-simple-suggest>
+    <p>Chosen element: {{ chosen }}</p>
+  </q-page>
+</template>
+
+<script>
+import VueSimpleSuggest from 'vue-simple-suggest'
+import 'vue-simple-suggest/dist/styles.css'
+
+export default {
+  name: 'PageIndex',
+  components: {
+    VueSimpleSuggest
+  },
+  data () {
+    return {
+      chosen: '',
+      items: ['Celular', 'Carro', 'Relógio', 'Pente', 'Buzina', 'Sabão']
+    }
+  },
+  methods: {
+    simpleSuggestionList () {
+      return this.items
+    }
+  }
+}
+</script>
